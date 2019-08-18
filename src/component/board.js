f62ce3c49d8565f077ac4ee2b9eb83c87ba99d16
singh_nishant <nishantnarayansingh135@gmail.com>
2019-08-18 21:51:52 +0530

    tictactoeinreact

---
+++ src/component/board.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Square from './square'
+//Board component 
+//----------------
+//Functional component that renders the entire board with all squares: 
+//- The board (3x3 grid)
+//- All squares components (passing with value and click handler for each one)
+//- Runs a click event handler on each individual square component
+class Board extends React.Component {
+    renderSquare(i) {
+      return (
+        <Square
+          value={this.props.squares[i]}
+          onClick={() => this.props.onClick(i)}
+        />
+      );
+    }
+  
+    render() {
+      return (
+        <div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+   
+  export default Board;
